081dfc3e32c516be7aa9a1074f218d7f9888738a
Dileep Annavarapu <dannavarapu@lenovo.com>
2023-05-31 11:31:00 +0530

    Implemented if and do

---
+++ impls/dlisp/step4_if_fn_do.js
@@ -0,0 +1,117 @@
+const readline = require('readline');
+const { read_str } = require('./reader.js');
+const { pr_str } = require('./printer.js');
+const { Env } = require('./env.js');
+
+const { MalSymbol, MalList, MalValue, MalVector, MalHashMap, MalNil } = require('./types.js');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const eval_ast = (ast, env) => {
+
+  if (ast instanceof MalSymbol) {
+    return env.get(ast);
+  }
+
+  if (ast instanceof MalList) {
+    const newAst = ast.value.map(x => EVAL(x, env));
+    return new MalList(newAst);
+  }
+
+  if (ast instanceof MalVector) {
+    const newAst = ast.value.map(x => EVAL(x, env));
+    return new MalVector(newAst);
+  }
+  if (ast instanceof MalHashMap) {
+    const newAst = ast.value.map(x => EVAL(x, env));
+    return new MalHashMap(newAst);
+  }
+
+  return ast;
+
+}
+
+const READ = str => read_str(str);
+
+const letBlock = (ast, env) => {
+  const bindings = ast.value[1].value;
+  const env2 = new Env(env);
+
+  for (let index = 0; index < bindings.length; index += 2) {
+    env2.set(bindings[index], EVAL(bindings[index + 1], env2));
+  }
+
+  if (ast.value.length < 3) {
+    return new MalNil();
+  }
+  return EVAL(ast.value[ast.value.length - 1], env2);
+}
+
+const ifBlock = (ast, env) => {
+  const condResult = EVAL(ast.value[1], env);
+
+  if (condResult.value != "nil" && condResult.value !== false) {
+    return EVAL(ast.value[2], env);
+  }
+  if (ast.value[3]) {
+    return EVAL(ast.value[3], env);
+  }
+  return new MalNil();
+}
+
+const defBlock = (ast, env) => {
+  env.set(ast.value[1], EVAL(ast.value[2], env));
+  return env.get(ast.value[1])
+}
+
+const doBlock = (ast, env) => {
+  const result = eval_ast(ast.value.slice(1), env);
+  return result[result.length - 1];
+}
+
+const EVAL = (ast, env) => {
+  if (!(ast instanceof MalList)) {
+    return eval_ast(ast, env);
+  }
+
+  if (ast.isEmpty()) {
+    return ast;
+  }
+
+  switch (ast.value[0].value) {
+    case "def!": return defBlock(ast, env);
+    case "let*": return letBlock(ast, env);
+    case "do": return doBlock(ast, env);
+    case "if": return ifBlock(ast, env);
+  }
+
+  const [fn, ...args] = eval_ast(ast, env).value;
+  return fn.apply(null, args);
+};
+
+const PRINT = malValue => malValue.toString();
+
+const env = new Env();
+
+env.set(new MalSymbol('+'), (...args) => args.reduce((a, b) => a + b));
+env.set(new MalSymbol('*'), (...args) => args.reduce((a, b) => a * b));
+env.set(new MalSymbol('/'), (...args) => args.reduce((a, b) => a / b));
+env.set(new MalSymbol('-'), (...args) => args.reduce((a, b) => a - b));
+
+const rep = str => PRINT(EVAL(READ(str), env));
+
+const repl = () =>
+  rl.question('user> ', line => {
+    try {
+      console.log(rep(line));
+
+    } catch (e) {
+      console.log(e);
+    }
+    repl();
+  });
+
+repl();
